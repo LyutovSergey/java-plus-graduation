51534b378d31d8de502dc49d517399d21a9e0dae
Sergey Lyutov <psvr2020@yandex.ru>
2026-08-28 00:10:47 +0300

    rev 0.3.1 Выделил request-service + общий модуль

--- core/rating-service/src/main/java/ru/practicum/rating/RatingServiceApp.java
+++ core/rating-service/src/main/java/ru/practicum/rating/RatingServiceApp.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableFeignClients
+@EnableFeignClients(basePackages = "ru.practicum.common.client")
 public class RatingServiceApp {
 
 	public static void main(String[] args) {
