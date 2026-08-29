320f6545f214b4638003002147effc7e71fe7562
Sergey Lyutov <psvr2020@yandex.ru>
2026-08-29 20:11:46 +0300

    rev 0.4b Выделение event-service

---
+++ core/event-service/src/main/java/ru/practicum/event/EventServiceApp.java
@@ -0,0 +1,19 @@
+package ru.practicum.event;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients(basePackages = {
+		"ru.practicum.common.client",
+		"ru.practicum.stat.client"
+})
+public class EventServiceApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventServiceApp.class, args);
+	}
+}
